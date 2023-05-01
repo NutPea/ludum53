53fdc26c68dabbb2d4b7310a0bd36891757ec000
NutPea <p.nuss96@outlook.de>
2023-05-01 22:50:25 +0200

    MARC IST SCHULD

--- Ludum Dare 53/Assets/Scripts/CarPassengerPickUpHandler.cs
+++ Ludum Dare 53/Assets/Scripts/CarPassengerPickUpHandler.cs
@@ -13,7 +13,7 @@ public class CarPassengerPickUpHandler : MonoBehaviour
     public Transform carPassengerPosition;
     [HideInInspector]public CustomerHandler currentCustomerHandler;
 
-    [HideInInspector] public UnityEvent<TargetLocationMarker> OnPickUpCustomer = new();
+    [HideInInspector] public UnityEvent OnPickUpCustomer = new();
     [HideInInspector] public UnityEvent OnDropCustomer = new();
     public UnityEvent<bool> OnHitEnemy = new();
 
